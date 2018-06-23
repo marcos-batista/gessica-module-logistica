3478752a46364dd455bf35d20b7af1dcf570b154
Marcos Batista <mab.marcos@gmail.com>
2018-06-23 17:14:09 -0300

    Ajuste dos objetos do modelo de dados

---
+++ src/main/java/com/agroall/gessica/logistica/dataobjects/Venda.java
@@ -0,0 +1,15 @@
+package com.agroall.gessica.logistica.dataobjects;
+
+public class Venda extends com.agroall.gessica.dataobjects.Venda {
+	
+	private EntregaVenda entregaVenda;
+	
+	public EntregaVenda getEntregaVenda() {
+		return entregaVenda;
+	}
+	
+	public void setEntregaVenda(EntregaVenda entregaVenda) {
+		this.entregaVenda = entregaVenda;
+	}
+	
+}
